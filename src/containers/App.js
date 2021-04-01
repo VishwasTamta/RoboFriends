5fe21aa75665a76a60d70eb97556316a708650e8
Vishwas Tamta <vishwastamta@gmail.com>
2021-04-01 23:14:41 +0530

    =Implemented react hooks in App.js file.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/cardList';
 import SearchBox from '../components/searchbox';
 import Scroll from '../components/Scroll';
 import './App.css';
 import ErrorBoundry from '../components/ErrorBoundry'
 
-class App extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchField: ''
+    //     }
+    // }
+    const [robots, SetRobots] = useState([]);
+    const [searchField, SetSearchField] = useState('');
+    const [count, setCount] = useState(0);
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({robots: users}))
+    // }
 
-    componentDidMount(){
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}))
-    }
+            .then(response => response.json())
+            .then(users => SetRobots(users))
+            console.log(count);
+    }, [count]) // Should remove count because it is rendering again and again by clicking Click me button
     
-    onSearchChange = (event) =>{
-        this.setState({searchField: event.target.value})
+    const onSearchChange = (event) =>{
+        SetSearchField(event.target.value)
     }
-
-    render(){
-        const {robots, searchField} = this.state;
         const filterRobots = robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
@@ -34,7 +40,8 @@ class App extends React.Component {
             (
                 <div className='tc'>
                     <h1 className='f1'>Robo Friends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <button onClick={()=> setCount(count+1)}>Click Me</button>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll >
                         <ErrorBoundry>
                             <CardList robots={filterRobots}/>
@@ -42,8 +49,6 @@ class App extends React.Component {
                     </Scroll>
                 </div>
             );
-            
-    }
 }
 
 export default App;
